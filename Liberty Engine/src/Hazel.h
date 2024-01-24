4d86f66c7475cc2e638c4b5cada353986e3acde3
Toto Tota <t.h.j.k@outlook.com>
2024-01-24 18:48:02 +0100

    Setup basic Application and entry point

---
+++ Liberty Engine/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Hazel Applications
+
+#include "Hazel/Application.h"
+
+// ---Entry Point--------------------------
+#include "Hazel/EntryPoint.h"
+// ----------------------------------------
---
+++ Liberty Engine/src/Hazel/Application.cpp
@@ -0,0 +1,15 @@
+#include "Application.h"
+
+namespace Hazel {
+	Application::Application() {
+
+	}
+
+	Application::~Application() {
+
+	}
+
+	void Application::Run() {
+		while (true);
+	}
+}
