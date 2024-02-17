f66416cdf45aabd3c60608227b50b71a71ae41cb
Toto Tota <t.h.j.k@outlook.com>
2024-02-17 19:46:14 +0100

    Vertex Arrays

--- Hazel/src/Hazel/Renderer/Buffer.h
+++ Hazel/src/Hazel/Renderer/Buffer.h
@@ -7,21 +7,21 @@ namespace Hazel{
 		None = 0, Float, Float2, Float3, Float4, Mat3, Mat4, Int, Int2, Int3, Int4, Bool
 	};
 
-	static uint32_t ShaderDataTypeSize(ShaderDataType type) 
+	static uint32_t ShaderDataTypeSize(ShaderDataType type)
 	{
 		switch (type)
 		{
-			case Hazel::ShaderDataType::Float:  return 4;
-			case Hazel::ShaderDataType::Float2: return 4 * 2;
-			case Hazel::ShaderDataType::Float3: return 4 * 3;
-			case Hazel::ShaderDataType::Float4: return 4 * 4;
-			case Hazel::ShaderDataType::Mat3:   return 4 * 3 * 3;
-			case Hazel::ShaderDataType::Mat4:   return 4 * 4 * 4;
-			case Hazel::ShaderDataType::Int:    return 4;
-			case Hazel::ShaderDataType::Int2:   return 4 * 2;
-			case Hazel::ShaderDataType::Int3:   return 4 * 3;
-			case Hazel::ShaderDataType::Int4:   return 4 * 4;
-			case Hazel::ShaderDataType::Bool:   return 1;
+		case Hazel::ShaderDataType::Float:  return 4;
+		case Hazel::ShaderDataType::Float2: return 4 * 2;
+		case Hazel::ShaderDataType::Float3: return 4 * 3;
+		case Hazel::ShaderDataType::Float4: return 4 * 4;
+		case Hazel::ShaderDataType::Mat3:   return 4 * 3 * 3;
+		case Hazel::ShaderDataType::Mat4:   return 4 * 4 * 4;
+		case Hazel::ShaderDataType::Int:    return 4;
+		case Hazel::ShaderDataType::Int2:   return 4 * 2;
+		case Hazel::ShaderDataType::Int3:   return 4 * 3;
+		case Hazel::ShaderDataType::Int4:   return 4 * 4;
+		case Hazel::ShaderDataType::Bool:   return 1;
 		}
 
 		HZ_CORE_ASSERT(false, "Unknown ShaderDataType!");
