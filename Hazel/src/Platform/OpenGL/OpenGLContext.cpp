29dbb2050e2d52d88e69237b0ad3adbb0cba8ad9
Toto Tota <t.h.j.k@outlook.com>
2024-02-18 18:29:16 +0100

    Added an orthographic camera

--- Hazel/src/Platform/OpenGL/OpenGLContext.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLContext.cpp
@@ -21,8 +21,8 @@ namespace Hazel {
 
 		HZ_CORE_INFO("OpenGL Info:");
 		HZ_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
-		HZ_CORE_INFO("  Vendor: {0}", glGetString(GL_RENDERER));
-		HZ_CORE_INFO("  Vendor: {0}", glGetString(GL_VERSION));
+		HZ_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		HZ_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
